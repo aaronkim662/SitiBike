2bb455f60ec7ef5933ee7d0fc2fcd12b1a2e5b33
Aaronkim662 <aaronkim662@gmail.com>
2019-08-23 09:13:18 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+
+
+class App extends React.Component {
+
+  makeApi = async() => {
+    let url = 'http://feeds.citibikenyc.com/stations/stations.json'
+    let data = await axios(url)
+    console.log(data)
+  }
+  componentDidMount() {
+    this.makeApi()
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
+}
 }
 
 export default App;
