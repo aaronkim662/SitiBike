c1f7772934679a11dd4d11e59f55650426d85cf9
Aaronkim662 <aaronkim662@gmail.com>
2019-08-29 14:42:50 -0400

    Updates

---
+++ precache-manifest.2aa084e3b8baaa56607091e29d80b2c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f50a34fac5a090c2d0d76ccb15a4558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b279b33f1a4e7fdf5b97",
+    "url": "/static/css/2.d844e46b.chunk.css"
+  },
+  {
+    "revision": "84882b6a792f932e15cf",
+    "url": "/static/css/main.c92b285e.chunk.css"
+  },
+  {
+    "revision": "b279b33f1a4e7fdf5b97",
+    "url": "/static/js/2.7393c1f3.chunk.js"
+  },
+  {
+    "revision": "84882b6a792f932e15cf",
+    "url": "/static/js/main.5bcf50ec.chunk.js"
+  },
+  {
+    "revision": "8be305c02c252602e339",
+    "url": "/static/js/runtime~main.216bf652.js"
+  },
+  {
+    "revision": "a1a70120096989a74c4978db7880d022",
+    "url": "/static/media/bike1.a1a70120.jpg"
+  },
+  {
+    "revision": "c088458c517aa95633068b73bd4d67e7",
+    "url": "/static/media/citi.c088458c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website_api/precache-manifest.d56e492e45874442afc22064e969a9c3.js"
+  "/precache-manifest.2aa084e3b8baaa56607091e29d80b2c2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website_api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
