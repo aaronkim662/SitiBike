881690e8242c0851bf1aa6f1cf585ab9baccbe8a
Aaronkim662 <aaronkim662@gmail.com>
2019-08-29 15:22:59 -0400

    Fixed an error

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header(){
   return (
     <nav className='headerNav' >
-        <ul><Link to='/home' className='ulist'>Home</Link ></ul>
+        <ul><Link to='/' className='ulist'>Home</Link ></ul>
         <ul><Link to="/Search" className='ulist'>Searching For a Bike</Link></ul>
     </nav>
   )
